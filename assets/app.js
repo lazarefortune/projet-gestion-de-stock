6fcf5ca28574bd79c6f87dfa5ebea9b20043abce
Lazare Fortune <lazarefortune@gmail.com>
2023-05-15 14:25:49 +0200

    config: add configuration setup

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/scss/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
